4fa98d694e07a5f89c51e798ed80bf4e4d62b077
Oguro Toshiyuki <oguro2124@gmail.com>
2025-07-02 14:54:49 +0900

    Initial commit

---
+++ StudentManagement1/src/test/java/raisetech/StudentManagement1/StudentManagement1ApplicationTests.java
@@ -0,0 +1,13 @@
+package raisetech.StudentManagement1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagement1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
