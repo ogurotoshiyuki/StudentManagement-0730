2d9f17ecb2cf3e06278fc8b189cfc6019a5dd7e1
Oguro Toshiyuki <ogurotoshiyuki2124@gmail.com>
2025-07-03 08:13:42 +0900

    コメント追記

--- StudentManagement1/src/main/java/raisetech/StudentManagement1/StudentManagement1Application.java
+++ StudentManagement1/src/main/java/raisetech/StudentManagement1/StudentManagement1Application.java
@@ -2,11 +2,18 @@ package raisetech.StudentManagement1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class StudentManagement1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagement1Application.class, args);
+	}
+	@GetMapping("/hello")
+	public String hello(){
+		return "Hello,Word!";//コメント練習
 	}
 }
