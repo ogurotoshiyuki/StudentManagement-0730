a6bec97b272959f9ef483cb77c821b773ca112a0
Oguro Toshiyuki <ogurotoshiyuki2124@gmail.com>
2025-07-09 21:20:23 +0900

    MyBatis課題要
    Merge remote-tracking branch 'origin/main'
    
    # Conflicts:
    #	StudentManagement1/src/main/java/raisetech/StudentManagement1/StudentManagement1Application.java

--- StudentManagement1/src/main/java/raisetech/StudentManagement1/StudentManagement1Application.java
+++ StudentManagement1/src/main/java/raisetech/StudentManagement1/StudentManagement1Application.java
@@ -4,9 +4,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,48 +19,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StudentManagement1Application {
 
-//	private String name = "Oguro　Toshiyuki";				//課題
-//	private String age = "64";											//課題
-
-	// ★ 蓄積用: List<Map>
-	private List<Map<String, String>> students = new ArrayList<>();
+		@Autowired																			//2025/07/09追加
+		private StudentRepository repository;						//2025/07/09追加
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagement1Application.class, args);
 	}
 	// GET: 全てのデータを返す
-	@GetMapping("/studentInfo")
-	public List<Map<String, String>> getStudentInfo() {
-		return students;
-//		return name + " " + age + "歳";							//課題
+	@GetMapping("/student")
+			public String getStudentInfo(@RequestParam String name){	//2025/07/09追加
+			Student student= repository.searchByName(name);						//2025/07/09追加
+//	public List<Map<String, String>> getStudentInfo() {					//2025/07/09削除
+		return student.getName()+" " + student.getAge() + "歳";			//2025/07/09追加
 	}
 	// POST: フォームデータを新しい Map にして List に追加
-	@PostMapping("/studentInfo") //POST追加（name&age）
-	public String setStudentInfo(
-			@RequestParam String name,
-			@RequestParam String age) {
-
-		Map<String, String> student = new HashMap<>();
-		student.put("name", name);
-		student.put("age", age);
-
-		students.add(student);
-
-		System.out.println("追加された student: " + student);
-		System.out.println("現在の students: " + students);
-
-		return "Student added: " + student;// 画面にも返す
+	@PostMapping("/student")
+	public void registerStudent(String name,int age){
+		repository.registerStudent(name,age);
 	}
-//	public void setName(String name,String age){//課題
-//		this.name = name;													//課題
-//		this.age = age;														//課題
-//	}																						//課題
-//	@PostMapping("/studentName")//POST追加(name)	//課題
-//	public void updateStudentName(String name){	//課題
-//		this.name = name;													//課題
-//	}																						//課題
-//	@PostMapping("/studentAge")//POST追加(age)		//課題
-//	public void updateStudentAge(String age){		//課題
-//		this.age = age;														//課題
-//	}																						//課題
+
+	@PatchMapping("/student")																		//2025/07/09追加
+	public void updateStudentName(String name,int age){					//2025/07/09追加
+		repository.updateStudent(name,age);												//2025/07/09追加
+	}
+
+	@DeleteMapping("/student")																	//2025/07/09追加
+	public void deleteStudent(String name){											//2025/07/09追加
+		repository.deleteStudent(name);														//2025/07/09追加
+	};
 }
