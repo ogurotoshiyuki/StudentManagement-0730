da00b13cdff201e642be06af8a17e43ac3c2870c
Oguro Toshiyuki <ogurotoshiyuki2124@gmail.com>
2025-07-10 08:43:33 +0900

    MyBatis演習課題➀  一覧表出力対応
    Merge remote-tracking branch 'origin/main'
    
    # Conflicts:
    #	StudentManagement1/src/main/java/raisetech/StudentManagement1/StudentManagement1Application.java

--- StudentManagement1/src/main/java/raisetech/StudentManagement1/StudentManagement1Application.java
+++ StudentManagement1/src/main/java/raisetech/StudentManagement1/StudentManagement1Application.java
@@ -1,9 +1,6 @@
 package raisetech.StudentManagement1;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,8 +8,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
