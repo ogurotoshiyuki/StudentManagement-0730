9b2374f0001851aeb626ba147a5040e40b2c5772
Oguro Toshiyuki <ogurotoshiyuki2124@gmail.com>
2025-07-30 10:22:57 +0900

    14_Thymeleafを使ったReadの画面描画処理（課題）

--- StudentManagement1/src/main/java/raisetech/StudentManagement1/controller/StudentController.java
+++ StudentManagement1/src/main/java/raisetech/StudentManagement1/controller/StudentController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import raisetech.StudentManagement1.controller.converter.StudentConverter;
@@ -12,29 +14,29 @@ import raisetech.StudentManagement1.data.StudentsCourses;
 import raisetech.StudentManagement1.domain.StudentDetail;
 import raisetech.StudentManagement1.service.StudentService;
 
-@RestController
+@Controller
 public class StudentController {
 
   private StudentService service;
   private StudentConverter converter;
 
   @Autowired
-  private StudentController(StudentService service, StudentConverter converter) {
+  public StudentController(StudentService service, StudentConverter converter) {
     this.service = service;
     this.converter = converter;
   }
 
-  @GetMapping(value = "/studentsList", produces = "application/json; charset=UTF-8")                                      //2025/07/10追加
-  public List<StudentDetail> getStudentList() {                      //2025/07/10追加
+  @GetMapping(value = "/studentList", produces = "application/json; charset=UTF-8")                                      //2025/07/10追加
+  public String getStudentList(Model model) {                      //2025/07/10追加
     List<Student> students = service.searchStudentList();
     List<StudentsCourses> studentsCourses = service.searchStudentsCoursesList();
-
-    return converter.convertStudentDetails(students, studentsCourses);
+    model.addAttribute("studentList",converter.convertStudentDetails(students,studentsCourses));
+    return "studentList";
+  }
+  @GetMapping("/studentsCoursesList")
+  public String getStudentsCoursesList(Model model) {
+    List<StudentsCourses> list = service.searchStudentsCoursesList();
+    model.addAttribute("coursesList", list);
+    return "studentsCoursesList"; // ← HTML名（拡張子は不要）
   }
 }
-
-//    @GetMapping("/studentsCourseList")
-//    public List<StudentsCourses> getStudentsCourseList() {
-//      return service.searchStudentsCourses();
-//    }
-//}
