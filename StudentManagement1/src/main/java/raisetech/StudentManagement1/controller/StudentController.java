c89b7a3185f632ba01cc39e5ef8f026ea2494a99
Oguro Toshiyuki <ogurotoshiyuki2124@gmail.com>
2025-07-30 12:04:10 +0900

    14_Thymeleafを使ったReadの画面描画処理（課題）

--- StudentManagement1/src/main/java/raisetech/StudentManagement1/controller/StudentController.java
+++ StudentManagement1/src/main/java/raisetech/StudentManagement1/controller/StudentController.java
@@ -6,7 +6,10 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import raisetech.StudentManagement1.controller.converter.StudentConverter;
 import raisetech.StudentManagement1.data.Student;
@@ -26,17 +29,35 @@ public class StudentController {
     this.converter = converter;
   }
 
-  @GetMapping(value = "/studentList", produces = "application/json; charset=UTF-8")                                      //2025/07/10追加
+  @GetMapping(value = "/studentList", produces = "application/json; charset=UTF-8")
+  //2025/07/10追加
   public String getStudentList(Model model) {                      //2025/07/10追加
     List<Student> students = service.searchStudentList();
     List<StudentsCourses> studentsCourses = service.searchStudentsCoursesList();
-    model.addAttribute("studentList",converter.convertStudentDetails(students,studentsCourses));
+    model.addAttribute("studentList", converter.convertStudentDetails(students, studentsCourses));
     return "studentList";
   }
+
   @GetMapping("/studentsCoursesList")
   public String getStudentsCoursesList(Model model) {
     List<StudentsCourses> list = service.searchStudentsCoursesList();
     model.addAttribute("coursesList", list);
     return "studentsCoursesList"; // ← HTML名（拡張子は不要）
+  }
+
+  @GetMapping("/newStudent")
+  public String newStudent(Model model) {
+    model.addAttribute("studentDetail", new StudentDetail());
+    return "registerStudent";
+  }
+
+  @PostMapping("/registerStudent")
+  public String registerStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result) {
+    if (result.hasErrors()) {
+      return "registerStudent";
+    }
+    System.out.println(
+        studentDetail.getStudent().getName() + "さんが新規受講生として登録されました。");
+    return "redirect:/studentList";
   }
 }
